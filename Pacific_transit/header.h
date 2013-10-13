a92ab38fc1235d6643ddcad37a8dcf8cd20322e9
Gabriel Vieira <viera_frifri@hotmail.com>
2013-10-13 18:28:50 -0300

    pasta nova

---
+++ Pacific_transit/header.h
@@ -0,0 +1,75 @@
+//
+//  Created by GABRIEL VIEIRA on 10/09/13.
+//  Copyright (c) 2013 GABRIEL VIEIRA. All rights reserved.
+//
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h> 
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_primitives.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> 
+#include <time.h>
+
+ //Define o tamanho da tela
+const int LARGURA_TELA = 800;
+const int ALTURA_TELA = 600;
+const int NUM_CARS = 5;
+
+//tamanho do carro   
+const float ALTURA_CARRO = 60;
+const float LARGURA_CARRO = 36;
+
+//limites da rua para printar na tela
+const float LIMITE_IMG_SUP = 220;
+const float LIMITE_IMG_INF = 290;
+
+//vetor de posições fixas da tela
+float positions[] = {245,298,353,415,470,523};
+float use_positions[] = {0,0,0,0,0,0};
+float car_speed[] = {1.2,0.7,1,0.3,1.1,0.5};
+int number_positions[] = {0,1,2,3,4,5};
+
+// Variaveis especiais do ALLEGRO 
+ALLEGRO_SAMPLE *som;
+ALLEGRO_DISPLAY *janela = NULL;
+ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
+ALLEGRO_BITMAP *fundo = NULL;
+ALLEGRO_BITMAP *menu = NULL;
+ALLEGRO_BITMAP *carro = NULL;
+ALLEGRO_BITMAP *carro2 = NULL;
+ALLEGRO_FONT *fonte = NULL;
+ALLEGRO_FONT *fonte2 = NULL;
+
+//struct para os carros
+typedef struct
+	{
+		ALLEGRO_BITMAP *image;
+
+		float imageX;
+		float imageY;
+		
+		float width;
+		float height;
+
+		float x;
+		float y;
+
+	}Image;
+	
+	typedef struct
+	{
+		Image image;
+		int position;
+		int number;
+		
+	}Car;
+
+bool inicializar();
+
+
---
+++ Pacific_transit/move.h
@@ -0,0 +1,126 @@
+//qq
+//  Created by GABRIEL VIEIRA on 10/09/13.
+//  Copyright (c) 2013 GABRIEL VIEIRA. All rights reserved.
+//
+
+void colision(Car *car_player , Car *auto_car)
+{ 
+    if ((auto_car->position == car_player->position) 
+    && ((auto_car->image.y - car_player->image.y) < 60 && (auto_car->image.y - car_player->image.y) > -60))
+    {
+       printf("Colidiu pista %d\n",auto_car->position );
+    }
+}
+
+void screen_limit(Car *car)
+{
+    if (car->image.y <= -60)
+    {
+       car->image.y = 600;
+    }
+}
+
+int RandomInteger( int low, int high)
+{
+    srand(time(NULL));
+
+    int k;
+    double d;
+    d = (double) rand( ) / ((double) RAND_MAX + 1);
+    k = d * (high - low + 1);
+    return low + k;
+}
+
+void index_position(int p , int n)
+{
+    int temp = number_positions[p];
+    number_positions[p] = number_positions[n];
+    number_positions[n] = temp;
+}
+
+void init_car(Car *car)
+{  
+    int r , temp;
+
+    r = RandomInteger(car->number, 5);
+    temp = r;
+   
+    printf("%d %d\n",car->number ,5);
+    temp = number_positions[temp];
+     index_position(car->number, r); 
+      
+
+    car->image.imageX = 15;
+    car->image.imageY = 202;
+    car->image.width = LARGURA_CARRO;
+    car->image.height = ALTURA_CARRO;
+    car->image.x = positions[temp];
+    car->image.y = LARGURA_TELA /2 - ALTURA_CARRO;
+    car->image.image = carro;
+    car->position = temp;
+
+    use_positions[temp] = 1;
+    //printf("%d\n",car->number );
+}
+
+void move_screen(float *telay)
+{
+    *telay -= 1;
+
+    if (*telay == 0)
+    {
+        *telay = 600;
+    }
+
+    al_draw_bitmap_region(fundo,0,*telay, LARGURA_TELA, ALTURA_TELA ,0,0,0);
+}
+
+void move_auto_car(Car *car)
+{
+    screen_limit(car);
+
+    car->image.y = car->image.y - car_speed[car->position];
+
+    al_draw_bitmap_region(carro2, car->image.imageX, car->image.imageY,
+     car->image.width , car->image.height , car->image.x ,car->image.y ,0);
+}
+
+void move_player_car(int tecla,Car *car,bool *move_permit)
+{
+
+    if (*move_permit == true)
+    {
+        
+        if (tecla)
+        {
+            switch (tecla)
+            {
+                case 3:
+                   
+                    if (car->position != 0)
+                    {
+                        car->position = car->position -1;
+                        car->image.x = positions[car->position];
+                    }
+                        
+                    *move_permit = false;
+                    break;
+
+                case 4:
+                    
+                    if (car->position != 5)
+                    {
+                        car->position = car->position +1;
+                        car->image.x = positions[car->position];
+                    } 
+                    
+                    *move_permit = false;
+                    break;
+            }
+
+        }
+    }       
+
+    al_draw_bitmap_region(car->image.image, car->image.imageX, car->image.imageY,
+     car->image.width , car->image.height , car->image.x ,car->image.y ,0);
+}
